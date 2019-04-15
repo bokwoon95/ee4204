be923f84373129630e182a2705267d5a7388e159
bokwoon <bokwoon.c@gmail.com>
2019-04-15 22:17:46 +0800

    everything still works 3, about to refactor

--- client.c
+++ client.c
@@ -2,47 +2,41 @@
 #include <arpa/inet.h>
 
 // Function Prototypes
-float sendtosocket2(int sockfd, struct sockaddr *addr, socklen_t addrlen, int *len);
+void sendtosocket(int sockfd, struct sockaddr *addr, socklen_t addrlen);
 void tv_sub(struct  timeval *out, struct timeval *in); //calculate the time interval between out and in
 
 int main(int argc, char *argv[]) {
-    /* Create socket config
-       struct fields must be sent in network-byte order (big endian)
+    /* Create internet socket address
+       struct fields must be in network-byte order (big endian)
        struct sockaddr_in {
-       short sin_family;          boilerplate=AF_INET
-       ushort sin_port;           TCP/UDP port
-       struct sin_addr {
-           ulong s_addr;          IPv4 address(es) you wish to send to
-       }
-       unsigned char[8] sin_zero; boilerplate=structure padding(initialize to all 0s)
+           short sin_family;          boilerplate=AF_INET
+           ushort sin_port;           TCP/UDP port
+           struct sin_addr {
+               ulong s_addr;          IPv4 address(es) you wish to send to
+           }
+           unsigned char[8] sin_zero; boilerplate=structure padding(initialize to all 0s)
        } */
-    struct sockaddr_in ser_addr;
-    ser_addr.sin_family = AF_INET;
-    ser_addr.sin_port = htons(MYUDP_PORT);
-    if (argc < 2) {
-        printf("Provide IP address to send to");
-        exit(1);
-    }
+    struct sockaddr_in addr_in;
+    addr_in.sin_family = AF_INET;
+    addr_in.sin_port = htons(MYUDP_PORT);
+    if (argc < 2) { printf("Provide IP_addr to send to"); exit(1); }
     char *IP_addr = argv[1];
-    ser_addr.sin_addr.s_addr = inet_addr(IP_addr);
-    bzero(&(ser_addr.sin_zero), 8);
+    addr_in.sin_addr.s_addr = inet_addr(IP_addr);
+    bzero(&(addr_in.sin_zero), 8);
+    // Typecast internet socket address to generic socket address
+    struct sockaddr *addr = (struct sockaddr *)&addr_in;
+    socklen_t addrlen = sizeof(struct sockaddr);
 
     // Create socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) { printf("error in socket"); exit(1); }
 
     // Send file data to socket
-    int len;
-    float time = sendtosocket2(sockfd, (struct sockaddr *)&ser_addr, sizeof(struct sockaddr_in), &len);
-    float transferrate = (len/(float)time);
-    printf("Time(ms) : %.3f, Data sent(byte): %d\nData rate: %f (Kbytes/s)\n", time, (int)len, transferrate);
-
-    // Close socket
+    sendtosocket(sockfd, addr, addrlen);
     close(sockfd);
-    exit(0);
 }
 
-float sendtosocket2(int sockfd, struct sockaddr *addr, socklen_t addrlen, int *len) {
+void sendtosocket(int sockfd, struct sockaddr *addr, socklen_t addrlen) {
     // Open file for reading
     char filename[] = "myfile.txt";
     FILE* fp = fopen(filename, "rt");
@@ -52,16 +46,15 @@ float sendtosocket2(int sockfd, struct sockaddr *addr, socklen_t addrlen, int *l
     fseek(fp, 0, SEEK_END); long filesize = ftell(fp); rewind(fp);
     printf("The file length is %d bytes\n", (int)filesize);
     printf("the data unit is %d bytes\n", PACKLEN);
-    char filebuffer[filesize];
+    char filebuffer[filesize]; // buffer used to contain the entire file
     /* Copy the file contents into filebuffer
     fread(void *ptr,        Buffer address
           size_t size,      Size of each element
           size_t count,     Number of elements to be copied
           FILE *stream      File input stream
-          );
-    */
+          ); */
     fread(filebuffer, 1, filesize, fp);
-    filebuffer[filesize]=0x4; // Append the filebuffer with the End of Transmission ASCII character
+    filebuffer[filesize]=0x4; // Append the filebuffer contents with the End of Transmission ASCII character
     filesize+=1; // Increase filesize by 1 byte because of the addition of the EOT ASCII character
     fclose(fp);
 
@@ -71,12 +64,14 @@ float sendtosocket2(int sockfd, struct sockaddr *addr, socklen_t addrlen, int *l
 
     struct ack_so ack;
     int counter = 0;
-    char sendbuffer[4*PACKLEN];
+    char sendbuffer[4*PACKLEN]; // buffer used to contain the outgoing packet
     long fileoffset = 0; // Tracks how many bytes have been sent so far
     while (fileoffset < filesize) {
         int packetsize = (PACKLEN < filesize - fileoffset) ? PACKLEN : filesize - fileoffset;
         printf("packetsize = %d\n", packetsize);
+        // Copy the next section of the filebuffer into the packet
         memcpy(sendbuffer, (filebuffer + fileoffset), packetsize);
+        // Send packet data into socket
         int n = sendto(sockfd, &sendbuffer, packetsize, 0, addr, addrlen);
         if (n < 0) printf("error in sending packet\n");
         if (counter % 4 == 0) {
@@ -86,7 +81,7 @@ float sendtosocket2(int sockfd, struct sockaddr *addr, socklen_t addrlen, int *l
             } else {
                 if (!(ack.num && !ack.len)) {
                     printf("Transmission Error\n");
-                    return -1;
+                    return;
                 }
             }
             printf("ACK received, alternating packet numbers\n");
@@ -94,15 +89,15 @@ float sendtosocket2(int sockfd, struct sockaddr *addr, socklen_t addrlen, int *l
         counter+=1;
         fileoffset += packetsize;
     }
-    *len = fileoffset;
 
     // Get end time
     struct timeval timeRcv;
     gettimeofday(&timeRcv, NULL);
+
+    // Calculate and print transfer rate
     tv_sub(&timeRcv, &timeSend);
-    float time_t = 0.0;
-    time_t += (timeRcv.tv_sec)*1000.0 + (timeRcv.tv_usec)/1000.0;
-    return time_t;
+    float time = (timeRcv.tv_sec)*1000.0 + (timeRcv.tv_usec)/1000.0;
+    printf("Time(ms) : %.3f, Data sent(byte): %d\nData rate: %.3f (Kbytes/s)\n", time, (int)fileoffset, fileoffset/time);
 }
 
 void tv_sub(struct  timeval *out, struct timeval *in) {
--- server.c
+++ server.c
@@ -1,54 +1,50 @@
 #include "headsock.h"
 
 // Function Protoypes
-void readfromsocket2(int sockfd);
+void readfromsocket(int sockfd);
 
 int main(int argc, char *argv[]) {
-    /* Create socket config
-       struct fields must be sent in network-byte order (big endian)
+    /* Create internet socket addr
+       struct fields must be in network-byte order (big endian)
        struct sockaddr_in {
-       short sin_family;          boilerplate=AF_INET
-       ushort sin_port;           TCP/UDP port
-       struct sin_addr {
-           ulong s_addr;          IPv4 address(es) you wish to receive from
-       }
-       unsigned char[8] sin_zero; boilerplate=structure padding(initialize to all 0s)
+           short sin_family;          boilerplate=AF_INET
+           ushort sin_port;           TCP/UDP port
+           struct sin_addr {
+               ulong s_addr;          IPv4 address(es) you wish to receive from
+           }
+           unsigned char[8] sin_zero; boilerplate=structure padding(initialize to all 0s)
        } */
-    struct sockaddr_in my_addr;
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(MYUDP_PORT);
-    my_addr.sin_addr.s_addr = INADDR_ANY; // INADDR_ANY=0
-    bzero(&(my_addr.sin_zero), 8);
+    struct sockaddr_in addr_in;
+    addr_in.sin_family = AF_INET;
+    addr_in.sin_port = htons(MYUDP_PORT);
+    addr_in.sin_addr.s_addr = INADDR_ANY; // INADDR_ANY=0, 0 means receive data from any IP address
+    bzero(&(addr_in.sin_zero), 8);
+    // Typecast internet socket address to generic socket address
+    struct sockaddr *addr = (struct sockaddr *)&addr_in;
+    socklen_t addrlen = sizeof(struct sockaddr);
 
     // Create socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) { printf("error in socket"); exit(1); }
 
-    // Bind socket config to socket
-    if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) == -1) {
+    // Bind socket config to socket (server only, client don't need)
+    if (bind(sockfd, addr, addrlen) == -1) {
         printf("error in binding");
         exit(1);
     }
 
     // Read file data from socket
-    while (1) {
-        readfromsocket2(sockfd);
-    }
-
-    // Close socket
+    while (1) { readfromsocket(sockfd); }
     close(sockfd);
-    exit(0);
 }
 
-void readfromsocket2(int sockfd) {
-    char filebuffer[BUFSIZE];
-    char recvbuffer[4*PACKLEN];
+void readfromsocket(int sockfd) {
+    char filebuffer[BUFSIZE]; // buffer used to contain the entire file
+    char recvbuffer[4*PACKLEN]; // buffer used to contain the incoming packet
 
-    // A & B are optional arguments for recvfrom/sendto, we don't care what their value is as long it's a sockaddr struct
-    struct sockaddr A;
-    struct sockaddr *Ap = &A;
-    socklen_t B = sizeof(struct sockaddr);
-    socklen_t *Bp = &B;
+    // create empty struct for the client address
+    struct sockaddr clientaddr;
+    socklen_t clientaddrlen = sizeof(struct sockaddr);
 
     struct ack_so ack;
     ack.num = 1;
@@ -61,7 +57,7 @@ void readfromsocket2(int sockfd) {
     // Keep reading until an End of Transmission character 0x4 is received
     do {
         if (acked) {
-            int bytesreceived = recvfrom(sockfd, &recvbuffer, 2*PACKLEN, 0, Ap, Bp);
+            int bytesreceived = recvfrom(sockfd, &recvbuffer, 2*PACKLEN, 0, &clientaddr, &clientaddrlen);
             if (bytesreceived < 0) printf("error in receiving packet\n");
             memcpy((filebuffer + fileoffset), recvbuffer, bytesreceived);
             fileoffset += bytesreceived;
@@ -69,7 +65,7 @@ void readfromsocket2(int sockfd) {
             packetlastbyte = recvbuffer[bytesreceived-1];
         }
         if (!acked) {
-            if (sendto(sockfd, &ack, 2, 0, Ap, B) < 0) {
+            if (sendto(sockfd, &ack, 2, 0, &clientaddr, clientaddrlen) < 0) {
                 printf("error sending ack\n");
             } else {
                 if (ack.num == 1 && ack.len == 0) {
@@ -77,6 +73,14 @@ void readfromsocket2(int sockfd) {
                 }
             }
         }
+        /* int bytesreceived = recvfrom(sockfd, &recvbuffer, 2*PACKLEN, 0, &clientaddr, &clientaddrlen); */
+        /* if (bytesreceived < 0) printf("error in receiving packet\n"); */
+        /* memcpy((filebuffer + fileoffset), recvbuffer, bytesreceived); */
+        /* fileoffset += bytesreceived; */
+        /* packetlastbyte = recvbuffer[bytesreceived-1]; */
+        /* int ack_sent = 0; */
+        /* while (!ack_sent) { */
+        /* } */
     } while (packetlastbyte != 0x4);
     fileoffset-=1; // Disregard the End of Transmission character 0x4
 
